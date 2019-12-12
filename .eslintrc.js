d01bcc68069e2c75b4e8cf5df67eca5959713d96
Belinda Vennam <beemarie@us.ibm.com>
2019-12-12 15:17:14 -0500

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+    }
+};
